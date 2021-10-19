ff60f7ad9aa31a5f2602c804c3a4645c1b4f6792
JanColon <50427965+JanColon@users.noreply.github.com>
2021-10-19 07:28:35 -0400

    Added DNA nucleotide support
    DNA nucleotide support is now added and a file was made to specifically handle the nucleotide chains (chain_handling.py)

---
+++ chain_handling.py
@@ -0,0 +1,281 @@
+from main import nc_out
+import acid_combos_rna
+import acid_combos_dna
+
+purgatory = False
+
+## Compares the user input to every array in 'acid_combos_rna.py' ##
+# Also outputs the respective amino acid associated with the chain # 
+
+for i in acid_combos_rna.F:
+	if i == nc_out:
+		print("Phenylalanine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.L:
+	if i == nc_out:
+		print("Leucine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.I:
+	if i == nc_out:
+		print("Isoleucine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.M:
+	if i == nc_out:
+		print("Methionine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.V:
+	if i == nc_out:
+		print("Valine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.S:
+	if i == nc_out:
+		print("Serine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.P:
+	if i == nc_out:
+		print("Proline")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.T:
+	if i == nc_out:
+		print("Threonine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.A:
+	if i == nc_out:
+		print("Alanine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.Y:
+	if i == nc_out:
+		print("Tyrosine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.H:
+	if i == nc_out:
+		print("Histidine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.Q:
+	if i == nc_out:
+		print("Glutamine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.N:
+	if i == nc_out:
+		print("Asparagine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.K:
+	if i == nc_out:
+		print("Lysine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.D:
+	if i == nc_out:
+		print("Aspartic acid")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.E:
+	if i == nc_out:
+		print("Glutamic acid")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.C:
+	if i == nc_out:
+		print("Cysteine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.W:
+	if i == nc_out:
+		print("Tryptophan")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.R:
+	if i == nc_out:
+		print("Arginine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.G:
+	if i == nc_out:
+		print("Glycine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.STP:
+	if i == nc_out:
+		print("Termination")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_rna.STR:
+	if i == nc_out:
+		print("Initiation")
+		purgatory = True
+	else:
+		pass
+
+### DNA Nucleotide Check ###
+
+for i in acid_combos_dna.F:
+	if i == nc_out:
+		print("Phenylalanine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.L:
+	if i == nc_out:
+		print("Leucine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.S:
+	if i == nc_out:
+		print("Serine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.Y:
+	if i == nc_out:
+		print("Tyrosine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.C:
+	if i == nc_out:
+		print("Cysteine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.W:
+	if i == nc_out:
+		print("Tryptophan")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.P:
+	if i == nc_out:
+		print("Proline")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.H:
+	if i == nc_out:
+		print("Histidine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.Q:
+	if i == nc_out:
+		print("Glutamine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.R:
+	if i == nc_out:
+		print("Arginine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.I:
+	if i == nc_out:
+		print("Isoleucine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.M:
+	if i == nc_out:
+		print("Methionine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.T:
+	if i == nc_out:
+		print("Threonine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.N:
+	if i == nc_out:
+		print("Asparagine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.K:
+	if i == nc_out:
+		print("Lysine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.V:
+	if i == nc_out:
+		print("Valine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.A:
+	if i == nc_out:
+		print("Alanine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.D:
+	if i == nc_out:
+		print("Aspartic acid")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.E:
+	if i == nc_out:
+		print("Glutamic acid")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.G:
+	if i == nc_out:
+		print("Glycine")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.STR:
+	if i == nc_out:
+		print("Initiation")
+		purgatory = True
+	else:
+		pass
+for i in acid_combos_dna.STP:
+	if i == nc_out:
+		print("Termination")
+		purgatory = True
+	else:
+		pass
+
+
+## Prevents program from closing ##
+while purgatory == True:
+	input()
